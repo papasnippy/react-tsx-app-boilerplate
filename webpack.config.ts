31b30588612effd2fd2806472d64d879627563ba
papasnippy <papasnippy@gmail.com>
2018-08-12 13:16:08 -0400

    Add extract text plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,12 @@
 import * as Path from 'path';
 import { Configuration } from 'webpack';
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 module.exports = (_env: any, options: any) => {
     const IS_PROD = options.mode === 'production';
+    const CHUNK_TYPE = IS_PROD ? 'chunkhash' : 'hash';
 
     return {
         mode: IS_PROD ? 'production' : 'development',
@@ -14,9 +15,18 @@ module.exports = (_env: any, options: any) => {
         },
         output: {
             path: Path.resolve(__dirname, 'build'),
-            filename: 'app.js'
+            filename: `content/[name].[${CHUNK_TYPE}].js`,
+            // publicPath: IS_PROD ? '/' + packageJson.name : '/'
+        },
+        resolve: {
+            alias: {}
         },
         plugins: [
+            new ExtractTextPlugin({
+                filename: `content/[name].[${CHUNK_TYPE}].css`,
+                disable: false,
+                allChunks: true
+            }),
             new HtmlWebpackPlugin({
                 template: Path.resolve(__dirname, './src/index.html'),
                 inject: 'body',
@@ -27,34 +37,37 @@ module.exports = (_env: any, options: any) => {
             rules: [
                 {
                     test: /(\.css$|\.scss$)/,
-                    use: [
-                        {
-                            loader: 'style-loader'
-                        },
-                        {
-                            loader: 'css-loader',
-                            query: {
-                                modules: true,
-                                minimize: false,
-                                localIdentName: '[name]__[local]___[hash:base64:5]'
+                    include: [
+                        Path.resolve(__dirname, './src')
+                    ],
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                query: {
+                                    modules: true,
+                                    minimize: false,
+                                    localIdentName: '[name]__[local]___[hash:base64:5]'
+                                }
+                            },
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            },
+                            {
+                                loader: 'resolve-url-loader'
+                            },
+                            {
+                                loader: 'sass-loader',
+                                query: {
+                                    sourceMap: true
+                                }
                             }
-                        },
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        },
-                        {
-                            loader: 'resolve-url-loader'
-                        },
-                        {
-                            loader: 'sass-loader',
-                            query: {
-                                sourceMap: true
-                            }
-                        }
-                    ]
+                        ]
+                    })
                 },
                 {
                     test: /\.tsx?$/,
